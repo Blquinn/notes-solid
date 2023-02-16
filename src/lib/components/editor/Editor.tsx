af854489d082299296977b915af086ab6044251e
Benjamin Quinn <benjamin.quinn92@gmail.com>
2023-02-16 11:06:06 -0500

    Move to title on backspace

--- src/lib/components/editor/Editor.tsx
+++ src/lib/components/editor/Editor.tsx
@@ -20,6 +20,7 @@ export default function Editor() {
 
   const [state, _] = useContext(NoteTreeContext);
 
+  let titleEl: HTMLInputElement | undefined;
   let editor: HTMLDivElement | undefined;
   let content: HTMLDivElement | undefined;
   let editorView: EditorView | undefined;
@@ -65,17 +66,35 @@ export default function Editor() {
     }
   }
 
+  const onEditorKey = (e: KeyboardEvent) => {
+    const selection = editorView?.state.selection;
+    if (e.key == 'Backspace' && selection?.empty && (selection?.$head.pos ?? -1) == 1) {
+      e.preventDefault();
+      titleEl?.focus();
+    }
+
+    // TODO: Fix Tab presses breaking editor. (Does focus move to another element?)
+    if (e.key == 'Tab') {
+      e.preventDefault();
+    }
+  }
+
   return (
     <>
       <input
         type="text"
+        ref={titleEl}
         class={`${styles.title} text-2xl bg-surface-50-900-token border-none`}
         placeholder="Note title..."
         value={title() ?? ''}
         onInput={onTitleInput}
         onKeyDown={onTitleKey}
       />
-      <div class={`${styles.editor} flex-1 flex flex-col overflow-y-auto min-h-0`} ref={editor}></div>
+      <div 
+        onKeyDown={onEditorKey}
+        ref={editor}
+        class={`${styles.editor} flex-1 flex flex-col overflow-y-auto min-h-0 bg-surface-50-900-token`} 
+      ></div>
       <div ref={content} class="hidden"></div>
     </>
   );
