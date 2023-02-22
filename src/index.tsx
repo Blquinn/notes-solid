8f3d46e82c749d0acf8c24877fac9ecc29421654
Benjamin Quinn <benjamin.quinn92@gmail.com>
2023-02-22 11:59:33 -0500

    Add resize observer

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,8 @@ import { render } from "solid-js/web";
 import "./style.css";
 import App from "./App";
 import 'simplebar';
+import ResizeObserver from 'resize-observer-polyfill';
+
+window.ResizeObserver = ResizeObserver;
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
